3df0d403007d753a10b33156ec2728d26f18bb04
pixel-brain <88598454+pixel-brain@users.noreply.github.com>
2022-06-17 19:28:20 -0600

    Color Schemes

--- Wireframe/Assets/Scripts/SettingsManager.cs
+++ Wireframe/Assets/Scripts/SettingsManager.cs
@@ -12,10 +12,23 @@ public class SettingsManager : MonoBehaviour
     public float[] maxAISpeed;
     public float[] raceLength;
     public int[] boostSpawnDist;
-    //[Header("Other settings")]
+    [Header("Color Schemes")]
+    public Color[] baseColor;
+    public Color[] glowColor;
+    public Color[] sunColor;
+    public Color[] skyColor1;
+    public Color[] skyColor2;
+
+    public Material baseMat;
+    public Material glowMat;
+    public Material sunMat;
+    public Material floorMat;
+    public Material skyMat;
 
     public static int difficulty;
     public static bool created;
+
+    int colorScheme;
 
     // Start is called before the first frame update
     void Start()
@@ -29,5 +42,29 @@ public class SettingsManager : MonoBehaviour
             created = true;
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    void Update()
+    {
+        //REMOVE THIS
+        if (Input.GetButtonDown("Jump"))
+        {
+            ChangeColor();
+        }
+    }
+
+    public void ChangeColor()
+    {
+        colorScheme++;
+        if (colorScheme > baseColor.Length - 1)
+        {
+            colorScheme = 0;
+        }
+        baseMat.SetColor("_BaseColor", baseColor[colorScheme]);
+        glowMat.SetColor("_EmissionColor", glowColor[colorScheme] * 4f);
+        sunMat.SetColor("_EmissionColor", sunColor[colorScheme] * 2.1f);
+        floorMat.SetColor("GridColor", glowColor[colorScheme] * 7f);
+        skyMat.SetColor("Color1", skyColor1[colorScheme]);
+        skyMat.SetColor("Color2", skyColor2[colorScheme]);
     }
 }
